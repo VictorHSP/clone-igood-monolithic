ffaeb20d4665e5a1d1acaae85b78a24242a36f9c
VictorHSP <victor.hugo.s@hotmail.com>
2022-09-03 11:55:56 -0300

    first commit

---
+++ src/main/java/com/clone/igood/CloneIgoodApplication.java
@@ -0,0 +1,13 @@
+package com.clone.igood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneIgoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneIgoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clone/igood/CloneIgoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.igood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneIgoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
